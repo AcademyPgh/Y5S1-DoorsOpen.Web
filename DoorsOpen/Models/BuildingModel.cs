41c4c23c93dff1de5f2db6cf64514550163b005c
NGalderisi <nathan@galderisi.com>
2021-05-06 10:51:36 -0400

    Adding BuildingModel and EventModel

---
+++ DoorsOpen/Models/BuildingModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoorsOpen.Models
+{
+	public class BuildingModel
+	{
+		public int Id { get; set; }
+		public string Building { get; set; }
+		public string Address1 { get; set; }
+		public string Address2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+		public bool WheelchairAccessible { get; set; }
+		public bool RestroomsAvailable { get; set; }
+		public bool WheelchairAccessibleRestroom { get; set; }
+		public bool PhotographyAllowed { get; set; }
+		public string StartTime { get; set; }
+		public string EndTime { get; set; }
+		public int Capacity { get; set; }
+		public string HistoricalOverview { get; set; }
+		public string VisitorExperience { get; set; }
+	}
+}
---
+++ DoorsOpen/Models/EventModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoorsOpen.Models
+{
+	public class EventModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public bool IsActive { get; set; }
+		public List<BuildingModel> Buildings { get; set; }
+	}
+}
