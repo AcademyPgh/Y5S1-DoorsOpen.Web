56d75d9c9f683a95a17fc3e4e1db7b7bd752a5f5
NGalderisi <nathan@galderisi.com>
2021-05-06 16:51:10 -0400

    Fixed checkboxes in Building/Create & Building/Edit. Moved all CSS changes to site.css. Added Display names to BuildingModel.

--- DoorsOpen/Models/BuildingModel.cs
+++ DoorsOpen/Models/BuildingModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DoorsOpen.Models
 {
@@ -9,19 +11,29 @@ namespace DoorsOpen.Models
 	{
 		public int Id { get; set; }
 		public string Building { get; set; }
+		[Display(Name = "Address")]
 		public string Address1 { get; set; }
+		[Display(Name = "Address 2")]
 		public string Address2 { get; set; }
 		public string City { get; set; }
 		public string State { get; set; }
 		public string Zip { get; set; }
+		[Display(Name = "Wheelchair Accessible")]
 		public bool WheelchairAccessible { get; set; }
+		[Display(Name = "Restrooms Available")]
 		public bool RestroomsAvailable { get; set; }
+		[Display(Name = "Wheelchair Accessible Restroom")]
 		public bool WheelchairAccessibleRestroom { get; set; }
+		[Display(Name = "Photography Allowed")]
 		public bool PhotographyAllowed { get; set; }
+		[Display(Name = "Start Time")]
 		public string StartTime { get; set; }
+		[Display(Name = "End Time")]
 		public string EndTime { get; set; }
 		public int Capacity { get; set; }
+		[Display(Name = "Historical Overview")]
 		public string HistoricalOverview { get; set; }
+		[Display(Name = "Visitor Experience")]
 		public string VisitorExperience { get; set; }
 	}
 }
