70839f87f8bbb98f824d3eb2650306d0fc8e1474
NGalderisi <nathan@galderisi.com>
2021-05-11 11:17:28 -0400

    Added API

---
+++ DoorsOpen/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoorsOpen.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DoorsOpen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly SiteDbContext _context;
+
+        public ValuesController(SiteDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public List<DoorsOpen.Models.BuildingModel> Get()
+        {
+            return _context.Buildings.ToList();
+        }
+    }
+}
